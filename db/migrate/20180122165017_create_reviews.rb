2f0704b52817809bb7b27056a1dcc059e8e260f1
Daniel Bacon <dbacon338@gmail.com>
2018-01-22 09:10:38 -0800

    add Movie, Review, Comment models; need to fill out seeds.rb to test

---
+++ db/migrate/20180122165017_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :username
+      t.text :review
+      t.text :comment
+      t.timestamps
+      t.references :movie
+    end
+  end
+end
