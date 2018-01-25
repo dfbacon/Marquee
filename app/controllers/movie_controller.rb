945ca3082eea4257b44ac59d8392ded307a6cd38
Daniel Bacon <dbacon338@gmail.com>
2018-01-25 09:53:09 -0800

    update movies and reviews schema; add movie show method; edit links on index page

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,4 +2,25 @@ class MovieController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @reviews = @movie.reviews
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create; end
+  def update; end
+  def destroy; end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:title, :release_date, :genre, :plot, :image)
+    end
 end
--- db/migrate/20180122165017_create_reviews.rb
+++ db/migrate/20180122165017_create_reviews.rb
@@ -3,7 +3,6 @@ class CreateReviews < ActiveRecord::Migration[5.1]
     create_table :reviews do |t|
       t.string :username
       t.text :review
-      t.text :comment
       t.timestamps
       t.references :movie
     end
