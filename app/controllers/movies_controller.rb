acca4a898465c891d51f8944f0daae6283b0dcd6
Daniel Bacon <dbacon338@gmail.com>
2018-01-31 14:46:42 -0800

    Updated seeds.rb; search tmdb working, need to add linking.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,14 @@ class MoviesController < ApplicationController
       redirect_to '/users/sign_up'
     else
       @movies = if params[:movie_title]
-        Movie.where('title LIKE ?', "%#{params[:movie_title]}%")
+        query = Movie.where('title LIKE ?', "%#{params[:movie_title]}%")
+        if query.blank?
+          @tmdb = true
+          url = Tmdb::Search.movie(params[:movie_title])
+          @URL = url
+        else
+          query
+        end
       else
         Movie.all.order('created_at DESC')
       end
