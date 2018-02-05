a744f649a5ab95ccf037a79761644c895d184f0a
Daniel Bacon <dbacon338@gmail.com>
2018-02-05 11:39:34 -0800

    review not saving

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   has_many :reviews
+  accepts_nested_attributes_for :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   has_many :comments
   belongs_to :movie
+  accepts_nested_attributes_for :movie
 end
