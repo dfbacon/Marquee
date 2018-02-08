4bf98747519ee2c8750a36af8c1243c637b07523
Daniel Bacon <dbacon338@gmail.com>
2018-02-08 08:03:21 -0800

    movie model tests refactored

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,26 +1,29 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
+  def setup
+    @movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', genre: 'Test', release_date: 'today')
+  end
+
   test 'valid movie' do
-    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', genre: 'Test', release_date: 'today')
-    assert movie.valid?
+    assert @movie.valid?
   end
 
   test 'invalid without title' do
-    movie = Movie.new(plot: 'test plot for testing.', genre: 'Test', release_date: 'today')
-    refute movie.valid?, 'movie is valid without a title'
-    assert_not_nil movie.errors[:title], 'no validation error for title present'
+    @movie.title = nil
+    refute @movie.valid?, 'movie is valid without a title'
+    assert_not_nil @movie.errors[:title], 'no validation error for title present'
   end
 
   test 'invalid without genre' do
-    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', release_date: 'today')
-    refute movie.valid?, 'movie is valid without a genre'
-    assert_not_nil movie.errors[:genre], 'no validation error for genre present'
+    @movie.genre = nil
+    refute @movie.valid?, 'movie is valid without a genre'
+    assert_not_nil @movie.errors[:genre], 'no validation error for genre present'
   end
 
   test 'invalid without release date' do
-    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', genre: 'Test')
-    refute movie.valid?, 'movie is valid without a release date'
-    assert_not_nil movie.errors[:release_date], 'no validation error for release date present'
+    @movie.release_date = nil
+    refute @movie.valid?, 'movie is valid without a release date'
+    assert_not_nil @movie.errors[:release_date], 'no validation error for release date present'
   end
 end
