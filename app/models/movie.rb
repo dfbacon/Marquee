b6d8be6b64a3fd468fba7c73d8af6a048dc1c687
Daniel Bacon <dbacon338@gmail.com>
2018-02-08 08:00:13 -0800

    update rails to 5.2 beta for minitest; added basic movie model testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, :genre, :release_date, presence: true
   has_many :reviews
   accepts_nested_attributes_for :reviews
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid movie' do
+    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', genre: 'Test', release_date: 'today')
+    assert movie.valid?
+  end
+
+  test 'invalid without title' do
+    movie = Movie.new(plot: 'test plot for testing.', genre: 'Test', release_date: 'today')
+    refute movie.valid?, 'movie is valid without a title'
+    assert_not_nil movie.errors[:title], 'no validation error for title present'
+  end
+
+  test 'invalid without genre' do
+    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', release_date: 'today')
+    refute movie.valid?, 'movie is valid without a genre'
+    assert_not_nil movie.errors[:genre], 'no validation error for genre present'
+  end
+
+  test 'invalid without release date' do
+    movie = Movie.new(title: 'Testing', plot: 'test plot for testing.', genre: 'Test')
+    refute movie.valid?, 'movie is valid without a release date'
+    assert_not_nil movie.errors[:release_date], 'no validation error for release date present'
+  end
 end
