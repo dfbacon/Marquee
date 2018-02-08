f80b52c1624f97ecc27ad0a2d32bc3eff57a8b96
Daniel Bacon <dbacon338@gmail.com>
2018-02-08 08:20:04 -0800

    add association test for movies and reviews

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -5,25 +5,28 @@ class MovieTest < ActiveSupport::TestCase
     @movie = movies(:valid)
   end
 
+  #MARK: - Movie model validation test set
   test 'valid movie' do
     assert @movie.valid?
   end
-
   test 'invalid without title' do
     @movie.title = nil
     refute @movie.valid?, 'movie is valid without a title'
     assert_not_nil @movie.errors[:title], 'no validation error for title present'
   end
-
   test 'invalid without genre' do
     @movie.genre = nil
     refute @movie.valid?, 'movie is valid without a genre'
     assert_not_nil @movie.errors[:genre], 'no validation error for genre present'
   end
-
   test 'invalid without release date' do
     @movie.release_date = nil
     refute @movie.valid?, 'movie is valid without a release date'
     assert_not_nil @movie.errors[:release_date], 'no validation error for release date present'
+  end
+
+  #MARK: - Association test set
+  test '#reviews' do
+    assert_equal 2, @movie.reviews.size
   end
 end
